c06fb5c3a099d0b31d3eb3900a7abca53cfc1607
Mattias Singmyr <mattias@grebban.com>
2022-12-04 11:50:49 +0100

    Solved 'camp-cleanup' at adventofcode.com

---
+++ advent-of-code/2022/4-camp-cleanup/one.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// https://adventofcode.com/2022/day/4
+int main(void) {
+    FILE *file = fopen("input.txt", "r");
+
+    int x[2] = {0,0};
+    int y[2] = {0,0};
+    int pairs = 0;
+    while(fscanf(file, "%d-%d,%d-%d", &x[0], &x[1], &y[0], &y[1]) == 4) {
+        if (x[0] >= y[0] && x[1] <= y[1]) ++pairs;
+        else if (y[0] >= x[0] && y[1] <= x[1]) ++pairs;
+    }
+
+    printf("%d\n", pairs);
+
+    fclose(file);
+
+    return 0;
+}
